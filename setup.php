8d0dc250eee3554cefa6f4846e2dc7dfa6ec4e8c
tsmr <thetsmr@gmail.com>
2022-01-29 11:41:30 +0100

    prepare rc2

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_TASKLISTS_VERSION', '2.0.0-rc1');
+define('PLUGIN_TASKLISTS_VERSION', '2.0.0-rc2');
 
 if (!defined("PLUGIN_TASKLISTS_DIR")) {
    define("PLUGIN_TASKLISTS_DIR", Plugin::getPhpDir("tasklists"));
