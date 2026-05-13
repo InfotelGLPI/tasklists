7755190f05f2043ae5a789b7c97e1558d96fc1e0
Thetsmr <thetsmr@gmail.com>
2026-05-13 19:14:53 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -37,7 +37,7 @@ use GlpiPlugin\Tasklists\Profile;
 use GlpiPlugin\Tasklists\Ticket;
 use GlpiPlugin\Tasklists\Task;
 
-define('PLUGIN_TASKLISTS_VERSION', '2.1.6');
+define('PLUGIN_TASKLISTS_VERSION', '2.1.7');
 
 if (!defined("PLUGIN_TASKLISTS_DIR")) {
     define("PLUGIN_TASKLISTS_DIR", Plugin::getPhpDir("tasklists"));
